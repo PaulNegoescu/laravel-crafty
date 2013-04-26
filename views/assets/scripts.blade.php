a2a1ed5a0fbb01bc6eacf62eb2375b3efb38c449
Paul Negoescu <Paul Negoescu>
2013-04-26 18:01:23 +0300

    Added session state to the accordion control

--- views/assets/scripts.blade.php
+++ views/assets/scripts.blade.php
@@ -1,27 +1,54 @@
 <script>
-	$('form h2')
-		/**
-		 * Handles clicks on the heading
-		 * @param  {[type]} e The event object
-		 */
+	var LC = {};
+
+	LC.headings = $('form h2');		
+	LC.toggledHeadings = sessionStorage.getItem("laravel.crafty.toggledHeadings");
+
+	if (LC.toggledHeadings) {
+		LC.toggledHeadings = LC.toggledHeadings.split(";");
+	} else {
+		LC.toggledHeadings = [0];
+	}
+	console.log(LC.toggledHeadings);
+
+	//when clicking on headings
+	LC.headings
 		.on('click', function(e) {
-			$(this)
+			var currentIndex = LC.headings.index(this),
+				i,
+				$this = $(this);
+
+			if ($.inArray(""+currentIndex, LC.toggledHeadings) == -1) {
+				console.log("It's new but will we add it?", ""+currentIndex);
+				//if the current index is not in the array we make sure to add it
+				LC.toggledHeadings.push(currentIndex);
+			} else {
+				//if the current index already exists in the array we remove it
+				console.log("We have it already: ", ""+currentIndex);
+				LC.toggledHeadings.splice(LC.toggledHeadings.indexOf(""+currentIndex), 1);
+				console.warn(LC.toggledHeadings, currentIndex, LC.toggledHeadings.indexOf(""+currentIndex));
+			}
+
+			//we save the new state to the session
+			sessionStorage.setItem("laravel.crafty.toggledHeadings", LC.toggledHeadings.join(";"))
+			console.log("Saved to the session: ", sessionStorage.getItem("laravel.crafty.toggledHeadings"));
+
+			$this
 				.toggleClass('active')
 				.next()
-				.slideToggle();
-		})
-		/**
-		 * We fetch the first sibbling div and hide it initially
-		 */
-		.next()
-			.hide()
-			.end()
-		/**
-		 * We jump back to the heading and select only the first one and make it visible
-		 */
-		.first()
+					.slideToggle();
+		});
+
+	/**
+	 * We fetch the first sibbling div and hide it initially
+	 */
+	LC.headings.next().hide();
+	
+	for(var i = 0; i < LC.toggledHeadings.length; i++) {
+		LC.headings.eq(LC.toggledHeadings[i])
 			.toggleClass('active')
 			.next()
-				.show()
-
+				.show();
+	}
+	
 </script>
